3b231a9cfc03b9b39b72bbb46d2a51d952e97969
El-minadero <kevinmendoza@icloud.com>
2019-01-25 12:52:49 -0700

    Master Update 1.0 . ANCOR project file structure defined with example tests inside

---
+++ ancor/tests/example_test.py
@@ -0,0 +1,17 @@
+from unittest import TestCase
+
+
+class TestTaper(TestCase):
+
+    def setup(self):
+        pass
+
+    def teardown(self):
+        pass
+
+    def test_case(self):
+        self.assertTrue(False)
+
+
+if __name__ == '__main__':
+    unittest.main()
