8f31f4e01b76573ee75b36e093f8d1cf70857c3d
El-minadero <kevinmendoza@icloud.com>
2019-01-29 13:41:25 -0700

    Addition 4: interfaces created for 3 different signal processing steps, but not hooked up to unit tests

---
+++ ancor/ancor/numpy_array_signal_functions.py
@@ -0,0 +1,25 @@
+
+def demean(arr,**kwargs):
+    return arr
+
+def resample(arr,**kwargs):
+    return arr
+
+def detrend(arr,**kwargs):
+    return arr
+
+def taper(arr,**kwargs):
+    return arr
+
+def whiten(arr,**kwawrgs):
+    return arr
+
+def t_normalize(arr,**kwargs):
+    return arr
+
+def bandpass(arr,**kwargs):
+    return arr
+
+def cross_correlate(arr1,arr2,**kwargs):
+    return None
+
---
+++ ancor/ancor/stream_signal_functions.py
@@ -0,0 +1,25 @@
+
+def demean(stream,**kwargs):
+    return stream
+
+def resample(stream,**kwargs):
+    return stream
+
+def detrend(stream,**kwargs):
+    return stream
+
+def taper(stream,**kwargs):
+    return stream
+
+def whiten(stream,**kwawrgs):
+    return stream
+
+def t_normalize(stream,**kwargs):
+    return stream
+
+def bandpass(stream,**kwargs):
+    return stream
+
+def cross_correlate(stream1,stream2,**kwargs):
+    return None
+
