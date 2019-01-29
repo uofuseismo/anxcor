2b3fd37e163e9c19ae599e8d60e07c808d11ecba
El-minadero <kevinmendoza@icloud.com>
2019-01-29 13:52:48 -0700

    Addition 2: created numpy array, vertical matrix, and stream signal processing files. Also connected bandpass filtering

--- ancor/tests/test_bandpass_filtering.py
+++ ancor/tests/test_bandpass_filtering.py
@@ -1,5 +1,8 @@
 import unittest
-class TestBandpassFiltering(unittest.TestCase):
+from ancor import numpy_array_signal_functions
+from ancor import numpy_vertical_matrix_signal_functions
+from ancor import stream_signal_functions
+class TestStreamBandpassFiltering(unittest.TestCase):
 
     def test_filter_frequency_out_of_band(self):
         """
@@ -10,7 +13,8 @@ class TestBandpassFiltering(unittest.TestCase):
         """
         source_tau_shift = 0.0
         target_tau_shift = 5.0
-        self.assertAlmostEqual(source_tau_shift,target_tau_shift,5,"test not implemented")
+        source = stream_signal_functions.bandpass(source_tau_shift)
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
 
     def test_filter_frequency_in_band(self):
         """
@@ -19,9 +23,9 @@ class TestBandpassFiltering(unittest.TestCase):
          currently not implemented
 
         """
-        source_tau_shift = 0.0
+        source = stream_signal_functions.bandpass(None)
         target_tau_shift = 5.0
-        self.assertAlmostEqual(source_tau_shift,target_tau_shift,5,"test not implemented")
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
 
     def test_phase_shift_not_introduced(self):
         """
@@ -30,19 +34,108 @@ class TestBandpassFiltering(unittest.TestCase):
          currently not implemented
 
         """
-        source_tau_shift = 0.0
+        source = stream_signal_functions.bandpass(None)
         target_tau_shift = 5.0
-        self.assertAlmostEqual(source_tau_shift,target_tau_shift,5,"test not implemented")
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
 
     def test_for_edge_effects(self):
         """
         ensure no gibbs phenomena
 
         """
-        source = 1
+        source = stream_signal_functions.bandpass(None)
         target = 0
         self.assertAlmostEqual(source,target,5,'not implemented')
 
+class TestNumpyBandpassFiltering(unittest.TestCase):
+
+    def test_filter_frequency_out_of_band(self):
+        """
+         tests to make sure that frequencies added outside the bandpassed range are not retained
+
+         currently not implemented
+
+        """
+        source_tau_shift = 0.0
+        target_tau_shift = 5.0
+        source = numpy_array_signal_functions.bandpass(source_tau_shift)
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
+
+    def test_filter_frequency_in_band(self):
+        """
+         tests to ensure frequencies are retained in band
+
+         currently not implemented
+
+        """
+        source = numpy_array_signal_functions.bandpass(None)
+        target_tau_shift = 5.0
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
+
+    def test_phase_shift_not_introduced(self):
+        """
+         tests to ensure phase shifts are not introduced to bandpass filter
+
+         currently not implemented
+
+        """
+        source = numpy_array_signal_functions.bandpass(None)
+        target_tau_shift = 5.0
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
+
+    def test_for_edge_effects(self):
+        """
+        ensure no gibbs phenomena
+
+        """
+        source =numpy_array_signal_functions.bandpass(None)
+        target = 0
+        self.assertAlmostEqual(source,target,5,'not implemented')
+
+class TestNumpyVerticalArrayBandpassFiltering(unittest.TestCase):
+
+    def test_filter_frequency_out_of_band(self):
+        """
+         tests to make sure that frequencies added outside the bandpassed range are not retained
+
+         currently not implemented
+
+        """
+        source_tau_shift = 0.0
+        target_tau_shift = 5.0
+        source = numpy_vertical_matrix_signal_functions.bandpass(source_tau_shift)
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
+
+    def test_filter_frequency_in_band(self):
+        """
+         tests to ensure frequencies are retained in band
+
+         currently not implemented
+
+        """
+        source = numpy_vertical_matrix_signal_functions.bandpass(None)
+        target_tau_shift = 5.0
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
+
+    def test_phase_shift_not_introduced(self):
+        """
+         tests to ensure phase shifts are not introduced to bandpass filter
+
+         currently not implemented
+
+        """
+        source =numpy_vertical_matrix_signal_functions.bandpass(None)
+        target_tau_shift = 5.0
+        self.assertAlmostEqual(source,target_tau_shift,5,"test not implemented")
+
+    def test_for_edge_effects(self):
+        """
+        ensure no gibbs phenomena
+
+        """
+        source =numpy_vertical_matrix_signal_functions.bandpass(None)
+        target = 0
+        self.assertAlmostEqual(source,target,5,'not implemented')
 
 
 if __name__ == '__main__':
