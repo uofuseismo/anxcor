8d68eac1fb6aa95d65bfb8b7556fda05a299d1d0
danedwells <danedwells@gmail.com>
2019-01-31 14:37:37 -0700

    Added demean unit test

---
+++ ancor/tests/test_demean.py
@@ -0,0 +1,42 @@
+import unittest
+
+class TestDemean(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_demean(self):
+        '''
+        Test for removing the mean for the whole file
+
+        Subtract target file from processed file: should equal zero
+
+        :return:
+
+
+
+        '''
+
+        source = 1
+        target = 0
+        self.assertAlmostEqual(source, target, 5, 'not implemented')
+
+    def test_signal_unaltered(self):
+        '''
+        Test for ensuring the signal is unaltered
+
+        subtract source file from processed file: should equal a constant (can be zero).
+        The result will be an array; if the array varies 'significantly' over its length, fail
+        will have to determine what is 'significantly'
+
+        :return:
+        '''
+
+        source = 1
+        target = 0
+        self.assertAlmostEqual(source, target, 5, 'not implemented')
+
+        
