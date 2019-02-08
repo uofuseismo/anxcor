6a70e5673eea20c47b4d7a7369a81552dffde3cf
El-minadero <kevinmendoza@icloud.com>
2019-02-08 10:21:54 -0700

    added specific unit tests for local database connection and handling.

--- ancor/tests/test_ancorbank.py
+++ ancor/tests/test_ancorbank.py
@@ -1,6 +1,19 @@
 import unittest
 
 
+class TestAncorBank(unittest.TestCase):
+
+    def test_files_in_bank(self):
+        self.assertFalse(True)
+
+    def test_day_span_appropriate(self):
+        self.assertFalse(True)
+
+    def test_generator_increment(self):
+        self.assertFalse(True)
+
+    def test_generator_amount(self):
+        self.assertFalse(True)
 
 
 
