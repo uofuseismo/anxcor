cde0873ae20ad73a6c3c487dfa8e843447c37797
El-minadero <kevinmendoza@icloud.com>
2019-03-27 16:15:25 -0600

    Merged all branches back to master. Im going to flag this revision as a0.0.0

---
+++ ancor/tests/test_bandpass_filtering.py
@@ -0,0 +1,139 @@
+import unittest
+from ancor import numpy_array_signal_functions
+from ancor import numpy_vertical_matrix_signal_functions
+from ancor import stream_signal_functions
+class TestStreamBandpassFiltering(unittest.TestCase):
+
+    def test_filter_frequency_out_of_band(self):
+        """
+         tests to make sure that frequencies added outside the bandpassed range are not retained
+
+         currently not implemented
+
+        """
+        target=0
+        source = stream_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_filter_frequency_in_band(self):
+        """
+         tests to ensure frequencies are retained in band
+
+         currently not implemented
+
+        """
+        source = stream_signal_functions.bandpass(None)
+        target = 0
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_phase_shift_not_introduced(self):
+        """
+         tests to ensure phase shifts are not introduced to bandpass filter
+
+         currently not implemented
+
+        """
+        target=0
+        source = stream_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_for_edge_effects(self):
+        """
+        ensure no gibbs phenomena
+
+        """
+        target=0
+        source = stream_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,'not implemented')
+
+class TestNumpyBandpassFiltering(unittest.TestCase):
+
+    def test_filter_frequency_out_of_band(self):
+        """
+         tests to make sure that frequencies added outside the bandpassed range are not retained
+
+         currently not implemented
+
+        """
+        target=0
+        source = numpy_array_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_filter_frequency_in_band(self):
+        """
+         tests to ensure frequencies are retained in band
+
+         currently not implemented
+
+        """
+        target=0
+        source = numpy_array_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_phase_shift_not_introduced(self):
+        """
+         tests to ensure phase shifts are not introduced to bandpass filter
+
+         currently not implemented
+
+        """
+        target=0
+        source = numpy_array_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_for_edge_effects(self):
+        """
+        ensure no gibbs phenomena
+
+        """
+        target = 0
+        source =numpy_array_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,'not implemented')
+
+class TestNumpyVerticalArrayBandpassFiltering(unittest.TestCase):
+
+    def test_filter_frequency_out_of_band(self):
+        """
+         tests to make sure that frequencies added outside the bandpassed range are not retained
+
+         currently not implemented
+
+        """
+        target = 0
+        source = numpy_vertical_matrix_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_filter_frequency_in_band(self):
+        """
+         tests to ensure frequencies are retained in band
+
+         currently not implemented
+
+        """
+        target=0
+        source = numpy_vertical_matrix_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_phase_shift_not_introduced(self):
+        """
+         tests to ensure phase shifts are not introduced to bandpass filter
+
+         currently not implemented
+
+        """
+        target=0
+        source =numpy_vertical_matrix_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,"test not implemented")
+
+    def test_for_edge_effects(self):
+        """
+        ensure no gibbs phenomena
+
+        """
+        target=0
+        source =numpy_vertical_matrix_signal_functions.bandpass(None)
+        self.assertAlmostEqual(source,target,5,'not implemented')
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ ancor/tests/test_process_stack.py
@@ -0,0 +1,99 @@
+import unittest
+from ancor import numpy_array_signal_functions
+from ancor import numpy_vertical_matrix_signal_functions
+from ancor import stream_signal_functions
+
+class TestSignalProcessingStackObspyStreams(unittest.TestCase):
+
+    def test_signal_processing_stack(self):
+        """
+         ensure the signal processing steps are what we expect
+
+         currently not implemented
+
+        """
+        target=0
+        source = stream_signal_functions.process_all(None)
+        self.assertAlmostEqual(source,target,5,"sig process steps not implemented")
+
+
+class TestSignalProcessingStackNumpyArray(unittest.TestCase):
+
+    def test_signal_processing_stack(self):
+        """
+         ensure the signal processing steps are what we expect
+
+         currently not implemented
+
+        """
+        target=0
+        source = numpy_array_signal_functions.process_all(None)
+        self.assertAlmostEqual(source,target,5,"sig process steps not implemented")
+
+
+class TestSignalProcessingStackNumpyMatrix(unittest.TestCase):
+
+    def test_signal_processing_stack(self):
+        """
+         ensure the signal processing steps are what we expect
+
+         currently not implemented
+
+        """
+        target=0
+        source = numpy_vertical_matrix_signal_functions.process_all(None)
+        self.assertAlmostEqual(source,target,5,"sig process steps not implemented")
+
+
+
+class TestCrossCorrelationStackObspyStream(unittest.TestCase):
+
+    def test_crosscorrelation_stack(self):
+        """
+         ensure the crosscorrelation steps including noise processing are what we expect
+
+         currently not implemented
+
+        """
+        target = 0
+        processed_data1 = stream_signal_functions.process_all(None)
+        processed_data2 = stream_signal_functions.process_all(None)
+        source          = stream_signal_functions.cross_correlate(processed_data1, processed_data2)
+        self.assertAlmostEqual(source, target, 5, "crosscorr steps not implemented")
+
+
+class TestCrossCorrelationStackNumpyArray(unittest.TestCase):
+
+    def test_crosscorrelation_stack(self):
+        """
+         ensure the crosscorrelation steps including noise processing are what we expect
+
+         currently not implemented
+
+        """
+        target = 0
+        processed_data1 = numpy_array_signal_functions.process_all(None)
+        processed_data2 = numpy_array_signal_functions.process_all(None)
+        source          = numpy_array_signal_functions.cross_correlate(processed_data1, processed_data2)
+        self.assertAlmostEqual(source, target, 5, "crosscorr steps not implemented")
+
+
+class TestCrossCorrelationStackNumpyArray(unittest.TestCase):
+
+    def test_crosscorrelation_stack(self):
+        """
+         ensure the crosscorrelation steps including noise processing are what we expect
+
+         currently not implemented
+
+        """
+        target = 0
+        processed_data1 = numpy_vertical_matrix_signal_functions.process_all(None)
+        processed_data2 = numpy_vertical_matrix_signal_functions.process_all(None)
+        source          = numpy_vertical_matrix_signal_functions.cross_correlate(processed_data1, processed_data2)
+        self.assertAlmostEqual(source, target, 5, "crosscorr steps not implemented")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
