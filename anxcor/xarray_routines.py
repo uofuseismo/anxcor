c1f7eab4b844fa9b6d31c302224bb435aac7ebd2
El-minadero <kevinmendoza@icloud.com>
2019-08-19 15:42:06 -0600

    added array length assertion. testing again

--- anxcor/xarray_routines.py
+++ anxcor/xarray_routines.py
@@ -63,7 +63,7 @@ class XArrayConverter(XArrayProcessor):
                     elevation, latitude, longitude = self._assign_coordinates(trace)
         empty_array = np.zeros((len(channels), len(stations), len(time_array)))
         for trace in stream:
-            chan = channels.index(trace.stats.channel)
+            chan       = channels.index(trace.stats.channel)
             station_id = stations.index(self._get_station_id(trace))
             assert len(time_array)==len(trace.data), 'time and trace data are not same length!!'
             empty_array[chan, station_id, :] = trace.data
