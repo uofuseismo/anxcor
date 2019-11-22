4f30fea0cdc53594ab7155de3d392b7e9a368770
El-minadero <kevinmendoza@icloud.com>
2019-11-22 12:55:50 -0700

    removing imports from init

--- anxcor/abstractions.py
+++ anxcor/abstractions.py
@@ -1,12 +1,34 @@
 import  anxcor.utils as utils
 from obspy.core import UTCDateTime
-import anxcor.constants as c
 import xarray as xr
 import pandas as pd
 import numpy as np
 import json
 
-sep_char = c.OPERATIONS_SEPARATION_CHARACTER
+TAPER_DEFAULT   =0.05
+RESAMPLE_DEFAULT=10.0
+UPPER_CUTOFF_FREQ=5.0
+LOWER_CUTOFF_FREQ=0.01
+MAX_TAU_DEFAULT=100.0
+FILTER_ORDER_BANDPASS=4
+SECONDS_2_NANOSECONDS = 1e9
+OPERATIONS_SEPARATION_CHARACTER = '->:'
+
+## t-norm constants
+T_NORM_TYPE='reduce_metric'
+T_NORM_ROLLING_METRIC= 'mean'
+T_NORM_REDUCE_METRIC = 'max'
+T_NORM_WINDOW=10.0
+T_NORM_LOWER_FREQ=0.001
+T_NORM_UPPER_FREQ=0.05
+
+## Whitening constants
+WHITEN_REDUCE_METRIC = None
+WHITEN_ROLLING_METRIC='mean'
+WHITEN_WINDOW_RATIO=0.01
+FILTER_ORDER_WHITEN=3
+WHITEN_TYPE='reduce_metric'
+sep_char = OPERATIONS_SEPARATION_CHARACTER
 def write(xarray, path, extension):
     array_path      = '{}{}{}{}'.format(path, utils.sep, extension, '.nc')
     data = xarray.copy()
